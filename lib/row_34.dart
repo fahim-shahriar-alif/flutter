89ea2ce5d40d8394a90d3736249378980b911fbb
Fahim Shahriar Alif <fahim.shahriar.alif@gmail.com>
2024-12-17 20:19:28 +0600

    row_34

--- lib/row_34.dart
+++ lib/row_34.dart
@@ -7,18 +7,56 @@ class Row34 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text("My Flutter Project"),
+          title: Center(child: const Text("My Flutter Project")),
         ),
-        body: Row(
-          children: [
-            Text('Asif',style: TextStyle(fontSize: 25),),
-            Text('Pranto',style: TextStyle(fontSize: 25)),
-            Text('Mukut',style: TextStyle(fontSize: 25)),
-            Text('Pranto',style: TextStyle(fontSize: 25)),
-            ElevatedButton(onPressed:(){
-            } , child: Text("Click me !!!!"),)
-          ],
-        )
+        body: Center(
+          child: Container(
+            width: 400,
+            height: 500,
+            // child: Row(
+            //   mainAxisAlignment: MainAxisAlignment.center,
+            //   crossAxisAlignment: CrossAxisAlignment.center,
+            //   children: [
+            //     Text('A',style: TextStyle(fontSize: 25),),
+            //     Text('S',style: TextStyle(fontSize: 25)),
+            //     Text('D',style: TextStyle(fontSize: 25)),
+            //     Text('F',style: TextStyle(fontSize: 25)),
+            //     ElevatedButton(onPressed:(){
+            //     } , child: Text("Click me !!!!"),)
+            //   ],
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ElevatedButton(onPressed:(){
+                      } , child: Text("1"),),
+                      ElevatedButton(onPressed:(){
+                      } , child: Text("2"),),
+                      ElevatedButton(onPressed:(){
+                      } , child: Text("3"),),
+                      ElevatedButton(onPressed:(){
+                      } , child: Text("4"),),
+
+
+                    ],
+                  ),
+                  ElevatedButton(onPressed:(){
+                  } , child: Text("1"),),
+                  ElevatedButton(onPressed:(){
+                  } , child: Text("2"),),
+                  ElevatedButton(onPressed:(){
+                  } , child: Text("3"),),
+                  ElevatedButton(onPressed:(){
+                  } , child: Text("4"),),
+
+                ],
+              ),
+          ),
+        ),
     );
   }
 }
